98a814a7c4b2f9d9cdcdf63340e0f19d6b4a440b
aamir0143 <mdaamirreza00@gmail.com>
2022-01-24 17:44:33 +0530

    [AamirOOPS] Add : Added Class And Objects

---
+++ OOPSConcepts/ClassAndObject/Dog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSConcepts.ClassAndObject
+{
+    public class Dog
+    {
+        //Charactersticks or Variables of entity
+        public string breed, color;
+        public int Age, size;
+        //Actions or Behavior or Methods of entity
+        public void Eat()
+        {
+            Console.WriteLine("Dog Eat");
+        }
+    }
+}
---
+++ OOPSConcepts/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using OOPSConcepts.ClassAndObject;
+
+using System;
+
+namespace OOPSConcepts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Class and Object Concepts.
+            Dog labsky = new Dog();
+            labsky.breed = "Labsky";
+            labsky.color = "White";
+            labsky.Age = 5;
+            labsky.size = 15;
+            labsky.Eat();//Call the Method
+            Console.WriteLine("Labsky details : breed:{0},color:{1},Age:{2},size{3}",labsky.breed,labsky.color,labsky.Age,labsky.size);
+            Console.ReadLine();
+        }
+    }
+}
